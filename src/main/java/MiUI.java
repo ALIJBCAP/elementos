d61ce15a2a6e292bb5e9adb82f8447d795ffeed1
T-107 <T-107@PC181.mshome.net>
2018-05-18 21:23:53 -0500

    Primera

---
+++ src/main/java/MiUI.java
@@ -0,0 +1,39 @@
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author T-107
+ */
+@SpringUI
+public class MiUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+      
+        VerticalLayout Layout=new VerticalLayout();
+        Label etiqueta=new Label("BIENBENIDO");
+                etiqueta.addStyleName(ValoTheme.LABEL_H2);
+        Button boton=new Button("ingresar");
+        boton.addStyleName(ValoTheme.BUTTON_DANGER);
+        //AGREGAMOS ETIQUETA Y BUTTON A LAYOUT
+        Layout.addComponent(etiqueta);
+        Layout.addComponent(boton);
+        
+        setContent(Layout);
+        
+    }
+    
+}
