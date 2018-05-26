324381e2bc15a8e0079a465a9130a8f3b16d3575
T-107 <T-107@PC186.mshome.net>
2018-05-25 21:48:12 -0500

    clase 4

---
+++ src/main/java/com/example/elementosmbc/Debito.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.elementosmbc;
+
+/**
+ *
+ * @author T-107
+ */
+public class Debito implements Tarjeta {
+    
+    @Override
+            
+            
+    public void obtenerSaldo(){
+        
+     System.out.println(" tu credito es de 6000");
+                    
+    }
+    
+}
--- src/main/java/com/example/elementosmbc/ElementosMbcApplication.java
+++ src/main/java/com/example/elementosmbc/ElementosMbcApplication.java
@@ -1,12 +1,22 @@
 package com.example.elementosmbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElementosMbcApplication {
-
+public class ElementosMbcApplication implements CommandLineRunner{
+@Autowired ServicioTarjeta servicio;
+@Autowired RepositorioMensaje repoMensaje;
 	public static void main(String[] args) {
 		SpringApplication.run(ElementosMbcApplication.class, args);
+                
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        servicio.obtenerSaldo().obtenerSaldo();
+        repoMensaje.save(new Mensaje("Mi primer mensaje"));
+    }
 }
