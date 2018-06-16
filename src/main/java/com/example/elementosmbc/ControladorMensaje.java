27168e131c3a4c31927d4351adb2ce259d7ac9bc
T-107 <T-107@PC185.mshome.net>
2018-06-15 21:43:36 -0500

    faltan 3

--- src/main/java/com/example/elementosmbc/ControladorMensaje.java
+++ src/main/java/com/example/elementosmbc/ControladorMensaje.java
@@ -5,8 +5,12 @@
  */
 package com.example.elementosmbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ControladorMensaje {
     
-    @GetMapping("/hola")
-    public String algo(){
-        return "Hola mundo de sde un controlador ";
+    
+    @Autowired RepositorioMensaje repoMensa;
+  //caso a Buscar todos   
+    @GetMapping("/mensaje")
+    public List<Mensaje> buscarTodos(){
+    return repoMensa.findAll();
     }
-}
+    
+    //caso b buscar por id
+       @GetMapping("/mensaje/{id}")
+    public Mensaje buscarPorId(@PathVariable String id){
+        Mensaje mensa=new Mensaje();
+        return repoMensa.findById(id).get();
+    }
+} 
+
+
+
